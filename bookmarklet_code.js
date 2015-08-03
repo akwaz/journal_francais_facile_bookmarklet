af895ff1958d100f1f2bd6d96126b5db29e684c1
akwaz <akwaz@users.noreply.github.com>
2015-08-03 19:09:21 +0200

    Fixed single-day bug
    Fixed a bug which occurs when yesterday we had a single-number date -
    like 3rd or 9th.
    
    Fixed by adding a simple if stmt which adds a 0 if it's needed

--- bookmarklet_code.js
+++ bookmarklet_code.js
@@ -1,6 +1,8 @@
 javascript:(function(){var date, day, month, year;date = new Date();day = date.getDate() - 1;half_month =  date.getMonth() + 1;
     month = '0' + half_month;
     year = date.getFullYear();
-   
+    if (day < 10) {
+        day = '0' + day;
+    }
     open('http://www.rfi.fr/emission/' + year + month + day +'-journal-francais-facile-' + day + month + year + '-20h00-20h10-tu/', '_self')
 })();
