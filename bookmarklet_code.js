af3345b136e19e2fabc9eacd14f24ab7ac1e71d6
Arek <akwaz@users.noreply.github.com>
2015-10-14 17:31:03 +0200

    Fixed month bug

--- bookmarklet_code.js
+++ bookmarklet_code.js
@@ -1,5 +1,5 @@
 javascript:(function(){var date, day, month, year;date = new Date();day = date.getDate() - 1;half_month =  date.getMonth() + 1;
-    month = '0' + half_month;
+    month = (half_month < 10) ? '0' + half_month : half_month;
     year = date.getFullYear();
     if (day < 10) {
         day = '0' + day;
